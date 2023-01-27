14af118778dec7d0e7a195c67d850542e1af0add
maedeh <m.khoshkam@middleeastbank.ir>
2023-01-27 16:36:29 +0330

    add @Inject annotation in MusicRemoteDataSource.kt

--- data/src/main/java/com/test/data/source/MusicRemoteDataSource.kt
+++ data/src/main/java/com/test/data/source/MusicRemoteDataSource.kt
@@ -4,8 +4,9 @@ import com.test.data.models.MusicParamsEntity
 import com.test.data.models.MusicResponseEntity
 import com.test.data.repository.MusicDataSource
 import com.test.data.repository.MusicRemote
+import javax.inject.Inject
 
-class MusicRemoteDataSource(private var remote: MusicRemote) : MusicDataSource {
+class MusicRemoteDataSource @Inject constructor(private var remote: MusicRemote) : MusicDataSource {
 
     override suspend fun getMusicList(params: MusicParamsEntity): MusicResponseEntity {
         return remote.getMusicList(params)
